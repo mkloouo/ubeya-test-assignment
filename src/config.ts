7786eb4fed7e913e762faa838ea12c3cdf941042
Mykola Odnosumov <mkloouo@gmail.com>
2021-11-25 19:50:01 +0300

    use env vars from react app

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,11 @@ export const config = {
   apis: {
     weather: {
       url: 'https://api.weatherbit.io/v2.0',
-      key: process.env.WEATHER_API_KEY,
+      key: process.env['REACT_APP_' + 'WEATHER_API_KEY'],
     },
     google: {
       url: 'https://maps.googleapis.com/maps/api',
-      key: process.env.GOOGLE_API_KEY,
+      key: process.env['REACT_APP_' + 'GOOGLE_API_KEY'],
     },
   },
 };
