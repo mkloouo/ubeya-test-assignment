7482bff5ce198b008fda1d22294e704d7605ba65
Mykola Odnosumov <mkloouo@gmail.com>
2021-11-25 19:50:22 +0300

    simplifyu

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,11 @@ export const config = {
   apis: {
     weather: {
       url: 'https://api.weatherbit.io/v2.0',
-      key: process.env['REACT_APP_' + 'WEATHER_API_KEY'],
+      key: process.env['REACT_APP_WEATHER_API_KEY'],
     },
     google: {
       url: 'https://maps.googleapis.com/maps/api',
-      key: process.env['REACT_APP_' + 'GOOGLE_API_KEY'],
+      key: process.env['REACT_APP_GOOGLE_API_KEY'],
     },
   },
 };
